089785e7ab18a3676b16b523267b28fc180efc23
tpcrispy <thomas.1993.smith@gmail.com>
2017-01-29 19:59:13 +1100

    re wrote code that to make it cleaner/easier to place document for later on

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 
+var  questionList = {
+   allQuestions: [],
+   addQuestions: function(questions, answers, correct){
+     this.allQuestions.push({
+       questions: questions,
+       answers: answers,
+       correct: correct
+     });
+   },
+   displayQuestions: function(randomNumber){
+     console.log(this.allQuestions[randomNumber].questions);
+     this.allQuestions[randomNumber].answers.forEach(function(ele, index){
+       console.log(index + ':' + ' ' + ele);
+     });
+     var promptAnswer = parseInt(prompt('What\'s your Answer?'));
+     this.checkAnswer(promptAnswer, randomNumber);
+   },
+   getRandomQuestion: function(){
+    // debugger;
+     var randomNumber = Math.floor((Math.random() * this.allQuestions.length));
+     this.displayQuestions(randomNumber);
+   },
+   checkAnswer: function(promptAnswer, randomNumber){
+    var checkAnswer = this.allQuestions[randomNumber].correct;
+     if (promptAnswer === checkAnswer) {
+       console.log('WINNER');
+     } else {
+       console.log('WRONG');
+     }
 
-function Question(questions, answers, correct) {
-  this.questions = questions;
-  this.answers = answers;
-  this.correct = correct;
+   }
 }
 
+
+
+questionList.addQuestions('Is Thomas awesome?', ['yes', 'no'], 0);
+questionList.addQuestions('Does my Internet Suck', ['yes', 'no'], 0);
+questionList.addQuestions('Will this work?', ['yes', 'no', 'Maybe'], 2);
+questionList.getRandomQuestion();
+
+
+
+/*
 Question.prototype.logQuestion = function () {
   console.log(this.questions);
     this.answers.forEach(function(i, index) {
@@ -21,7 +57,6 @@ Question.prototype.checkAnswer = function (alert, callback) {
   } else {
     console.log('wrong answer');
     sc = callback(false);
-
   }
   this.displayScore(sc);
 }
@@ -30,11 +65,11 @@ console.log("Your current score is: " + score);
 console.log('---------------------------------');
 };
 
+
 var q1 = new Question('Is Thomas awesome?', ['yes', 'no'], 0);
 var q2 = new Question('Who is more awesome out of Thomas or Samantha?', ['Samantha', 'Thomas', 'Other'], 1);
 var q3 = new Question('Does this internet suck?', ['yes', 'no'], 0);
 
-var allQuestions = [q1, q2, q3];
 
 function score(){
   var sc = 0;
@@ -60,4 +95,18 @@ function continueGame() {
   }
 }
 
-continueGame();
+//continueGame();
+var view = {
+  displayAnswers: function(){
+    var answersUL = document.querySelector('ul');
+    answersUL.innerHTML = '';
+    allQuestions.forEach(function(ele, index, arr){
+      var answerLI = document.createElement('li');
+      answerLI.id = index;
+      answerLI.textContent = ele;
+      answersUL.appendChild(answerLI);
+
+    });
+      }
+    }
+*/
